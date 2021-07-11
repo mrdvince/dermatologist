1473e8e5ea0fffb0b4f8428fce865426a9fade23
Vince <vincivenv@gmail.com>
2021-07-11 04:04:37 +0300

    feat: Process images and labels

--- include/dataset.h
+++ include/dataset.h
@@ -6,6 +6,6 @@
 
 torch::Tensor read_data(std::string image_path);
 torch::Tensor read_labels(int label);
-// std::vector<torch::Tensor> process_images(std::vector<std::string> list_of_images);
-// std::vector<torch::Tensor> process_labels(std::vector<int> list_of_labels);
+std::vector<torch::Tensor> process_images(std::vector<std::string> list_of_images);
+std::vector<torch::Tensor> process_labels(std::vector<int> list_of_labels);
 std::pair<std::vector<std::string>, std::vector<int>> load_data_from_folder(std::vector<std::string> folder_names);
--- src/dataset.cpp
+++ src/dataset.cpp
@@ -4,19 +4,31 @@ torch::Tensor read_data(std::string image_path) {
     cv::Mat img = cv::imread(image_path, 1);
     cv::resize(img, img, cv::Size(224, 224), cv::INTER_CUBIC);
     torch::Tensor img_tensor = torch::from_blob(img.data, {img.rows, img.cols, 3}, torch::kByte);
-    img_tensor = img_tensor.permute({2, 0, 1});
+    img_tensor = img_tensor.permute({2, 0, 1});  // convert to channel, height, width -> (e.g 3,224,224)
     return img_tensor.clone();
 };
 
-torch::Tensor read_labels(int label) {
+torch::Tensor read_label(int label) {
     torch::Tensor label_tensor = torch::full({1}, label);
-    return label_tensor.clone();  // not sure if this is necessary since main reason for cloning the images tensors is the underlying shared memory
+    return label_tensor;
 };
 
-// std::vector<torch::Tensor> process_images(std::vector<std::string> list_of_images){
-
-// };
-// std::vector<torch::Tensor> process_labels(std::vector<int> list_of_labels);
+std::vector<torch::Tensor> process_images(std::vector<std::string> list_of_images) {
+    std::vector<torch::Tensor> images;
+    for (std::vector<std::string>::iterator it = list_of_images.begin(); it != list_of_images.end(); ++it) {
+        torch::Tensor image = read_data(*it);
+        images.push_back(image);
+    }
+    return images;
+};
+std::vector<torch::Tensor> process_labels(std::vector<int> list_of_labels) {
+    std::vector<torch::Tensor> labels;
+    for (std::vector<int>::iterator it = list_of_labels.begin(); it != list_of_labels.end(); ++it) {
+        torch::Tensor label = read_label(*it);
+        labels.push_back(label);
+    }
+    return labels;
+};
 std::pair<std::vector<std::string>, std::vector<int>> load_data_from_folder(std::vector<std::string> folder_names) {
     std::vector<std::string> list_of_images;
     std::vector<int> list_of_labels;
