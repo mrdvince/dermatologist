8cb9a340489bbc9d88d312fb71b2216a40ba21cd
Vince <vincivenv@gmail.com>
2021-07-14 06:57:46 +0300

    feat: Train, validate on validation set

--- src/trainer.cpp
+++ src/trainer.cpp
@@ -1,63 +1,70 @@
 #include "trainer.h"
 
-template <typename Dataloader>
-void train(torch::jit::script::Module module,
-           torch::nn::Linear linear,
-           Dataloader &train_loader,
-           Dataloader &valid_loader,
-           torch::optim::Optimizer &optimizer,
-           size_t dataset_size) {
-
+template <typename Trainloader, typename Validloader>
+void trainer(torch::jit::script::Module net,
+             torch::nn::Linear lin,
+             Trainloader &data_loader,
+             Validloader &valid_loader,
+             torch::optim::Optimizer &optimizer,
+             size_t dataset_size) {
     float valid_loss = 0.0;
     float valid_loss_min = std::numeric_limits<float>::infinity();
-    float train_loss = 0.0, acc = 0.0;
-    for (int i = 0; i < 25; i++) {
-        for (auto &batch : *train_loader) {
-            auto data = batch.data;
-            auto targets = batch.target.squeeze();
+    float train_loss = 0.0;
 
+    for (int i = 0; i < 25; i++) {
+        for (auto &batch : *data_loader) {
+            auto data = batch.data;
+            auto target = batch.target.squeeze();
             data = data.to(torch::kF32);
-            targets = data.to(torch::kInt64);
+            target = target.to(torch::kInt64);
             std::vector<torch::jit::IValue> input;
             input.push_back(data);
             optimizer.zero_grad();
-            auto output = module.forward(input).toTensor();
+            auto output = net.forward(input).toTensor();
+            // For transfer learning
             output = output.view({output.size(0), -1});
-            output = linear(output);
-
-            auto loss = torch::cross_entropy_loss(torch::softmax(output, 1), targets);
+            output = lin(output);
+            auto loss = torch::nll_loss(torch::log_softmax(output, 1), target);
             loss.backward();
             optimizer.step();
-
-            // auto acc = output.argmax(1).eq(targets).sum();
-            // acc += acc.template item<float>();
             train_loss += loss.template item<float>();
         }
-        module.eval();
+        net.eval();
+        int correct = 0, total = 0;
         for (auto &batch : *valid_loader) {
             auto data = batch.data;
-            auto targets = batch.target.squeeze();
+            auto target = batch.target.squeeze();
             data = data.to(torch::kF32);
-            targets = data.to(torch::kInt64);
+            target = target.to(torch::kInt64);
             std::vector<torch::jit::IValue> input;
             input.push_back(data);
-            auto output = module.forward(input).toTensor();
+            auto output = net.forward(input).toTensor();
+
             output = output.view({output.size(0), -1});
-            output = linear(output);
-            auto loss = torch::cross_entropy_loss(torch::softmax(output, 1), targets);
-            auto acc = output.argmax(1).eq(targets).sum();
+            output = lin(output);
+            auto loss = torch::nll_loss(torch::log_softmax(output, 1), target);
             valid_loss += loss.template item<float>();
+
+            auto out_tuple = torch::max(output, 1);
+            auto predicted = std::get<1>(out_tuple);
+            total += target.size(0);
+            correct += (predicted == target).sum().template item<int>();
         }
+
         train_loss = train_loss / dataset_size;
         valid_loss = valid_loss / dataset_size;
-        std::cout << "Epoch: " << i << ", "
-                  << "Training Loss: " << train_loss << std::endl;
+
+        std::cout << "Epoch: " << i
+                  << "\tTraining Loss: " << train_loss
+                  << "\tValidation Loss: " << valid_loss
+                  << "\tValidation Accuracy: " << (100 * correct / total) << " %" << std::endl;
+
         // save model if validation loss has decreased
         if (valid_loss <= valid_loss_min) {
-            std::cout << "Validation loss decreased from: " << valid_loss_min << "-->" << valid_loss << "\nSaving model ...";
+            std::cout << "Validation loss decreased from: " << valid_loss_min << " --> " << valid_loss << "\nSaving model ...";
             valid_loss_min = valid_loss;
-            module.save("module_model.pt");
-            torch::save(linear, "model_linear.pt");
+            net.save("module_model.pt");
+            torch::save(lin, "model_linear.pt");
         }
     }
 }
