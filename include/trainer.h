fab86ba6aa6c43b95498bd6f4af1da4b2abbaba4
Vince <vincivenv@gmail.com>
2021-07-14 06:57:30 +0300

    trainer: Update trainer header file

--- include/trainer.h
+++ include/trainer.h
@@ -1,9 +1,17 @@
+#ifndef trainer_h
+#define trainer_h
+
+#include <dirent.h>
+#include <torch/script.h>
 #include <torch/torch.h>
 
-template <typename Dataloader>
-void train(torch::jit::script::Module module,
-           torch::nn::Linear linear,
-           Dataloader &train_loader,
-           Dataloader &valid_loader,
-           torch::optim::Optimizer &optimizer,
-           size_t dataset_size);
+#include <iostream>
+#include <opencv2/opencv.hpp>
+template <typename Trainloader, typename Validloader>
+void trainer(torch::jit::script::Module net,
+             torch::nn::Linear lin,
+             Trainloader &data_loader,
+             Validloader &valid_loader,
+             torch::optim::Optimizer &optimizer,
+             size_t dataset_size);
+#endif
